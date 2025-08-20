582e78e7b631d4a02350c1e384097dde298e760e
nmatondo <nmatondo@student.42luanda.com>
2025-08-20 10:14:20 +0100

    Add algoritimo de criptografia e tempo de duracao do aceess token

--- app/encryption.py
+++ app/encryption.py
@@ -4,4 +4,6 @@ from passlib.context import CryptContext
 
 load_dotenv()
 SECRET_KEY =  os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
