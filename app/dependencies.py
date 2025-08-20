37e2c5f31e4c270a7897c766d042474c822bc8c6
nmatondo <nmatondo@student.42luanda.com>
2025-08-20 12:59:54 +0100

    Dependencia verificar token add

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,5 +1,11 @@
 from models import db
-from sqlalchemy.orm import sessionmaker
+from models import Users
+from fastapi import Depends, HTTPException
+from sqlalchemy.orm import sessionmaker, Session
+from jose import jwt, JWTError
+from encryption import SECRET_KEY, ALGORITHM, oauth2_schema
+
+
 
 def init_session():
     try:
@@ -8,3 +14,16 @@ def init_session():
         yield session
     finally:
         session.close()
+
+
+def verify_token(token: str = Depends(oauth2_schema), session: Session =  Depends(init_session)):
+
+    try:
+        info = jwt.decode(token, SECRET_KEY, ALGORITHM)
+        id_user = info.get('sub')
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Access denied")
+    user = session.query(Users).filter(Users.id == id_user).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
