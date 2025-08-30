d015fdccc2829f8f2cabbc486d18efcd4c858175
NdDaniel2020 <ndondadaniel2020@gmail.com>
2025-08-30 19:14:33 +0100

    New date

--- start_all.sh
+++ start_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 tmux new-session -d -s infrawatch 'python api/app.py'
-tmux split-window -h -t infrawatch 'sudo /home/ubuntu/Code/infrawatch/infrawatch-backend/venv/bin/python3 monitor/monitor.py'
-tmux split-window -h -t infrawatch 'cd ../infrawatch-frontend/ && npm run dev'
-tmux select-layout -t infrawatch even-horizontal
+tmux split-window -v -t infrawatch 'sudo /home/ubuntu/Code/infrawatch/infrawatch-backend/venv/bin/python3 monitor/monitor.py'
+tmux split-window -v -t infrawatch 'cd ../infrawatch-frontend/ && npm run dev'
+tmux select-layout -t infrawatch even-vertical
 tmux attach -t infrawatch
